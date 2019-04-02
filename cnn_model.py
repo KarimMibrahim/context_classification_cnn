15e02a7d0d56bb9f6f42ea17d6db18e7157e1cf5
Karim <karim.m.ibrahim@gmail.com>
2019-04-02 15:05:16 +0200

    docker working

--- cnn_model.py
+++ cnn_model.py
@@ -14,26 +14,29 @@ from dzr_ml_tf.label_processing import tf_multilabel_binarize
 from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 
 # Machine Learning preprocessing and evaluation
-from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report, roc_auc_score, hamming_loss
+from sklearn.metrics import accuracy_score, precision_score, recall_score, classification_report, roc_auc_score, \
+    hamming_loss
 from sklearn.model_selection import train_test_split
 
+SOURCE_PATH = "/my_data/"
+SPECTROGRAMS_PATH = "/specs/"
 
-base_datasets_path = "/my_data/"
-INPUT_SHAPE = (1292,96, 1)
-label_list = ['car','chill','club','dance','gym','happy','morning','night','park','party','relax','running','sad',
-              'shower','sleep','summer','train','training','work','workout']
+INPUT_SHAPE = (1292, 96, 1)
+LABELS_LIST = ['car', 'chill', 'club', 'dance', 'gym', 'happy', 'morning', 'night', 'park', 'party', 'relax', 'running',
+               'sad',
+               'shower', 'sleep', 'summer', 'train', 'training', 'work', 'workout']
 
 
-def split_dataset(csv_path = os.path.join(base_datasets_path,"GroundTruth/ground_truth_single_label.csv"),
-                  test_size=0.25, seed = 0, save_csv = True ,
-                  train_save_path = os.path.join(base_datasets_path,"GroundTruth/train_ground_truth.csv"),
-                  test_save_path = os.path.join(base_datasets_path,"GroundTruth/test_ground_truth.csv")):
+def split_dataset(csv_path=os.path.join(SOURCE_PATH, "GroundTruth/ground_truth_single_label.csv"),
+                  test_size=0.25, seed=0, save_csv=True,
+                  train_save_path=os.path.join(SOURCE_PATH, "GroundTruth/train_ground_truth.csv"),
+                  test_save_path=os.path.join(SOURCE_PATH, "GroundTruth/test_ground_truth.csv")):
     groundtruth = pd.read_csv(csv_path)
     train, test = train_test_split(groundtruth, test_size=test_size, random_state=seed)
     if save_csv:
         pd.DataFrame.to_csv(train, train_save_path, index=False)
         pd.DataFrame.to_csv(test, test_save_path, index=False)
-    return train,test
+    return train, test
 
 
 def load_spectrogram_tf(sample, identifier_key="song_id",
@@ -63,20 +66,19 @@ def load_spectrogram(*args):
             Error: boolean, False if no error, True if an error was raised during features computation.
     """
     # TODO: edit path
-    path = os.path.join(base_datasets_path,"/MelSpectograms_top20/")
+    path = SPECTROGRAMS_PATH
     song_id, dummy_path = args
     try:
         # tf.logging.info(f"Load spectrogram for {song_id}")
         spect = np.load(os.path.join(path, str(song_id) + '.npz'))['feat']
         if (spect.shape != (1, 1292, 96)):
-            # tf.logging.warn(f"Error while computing features for {song_id}")
+            print("Error while computing features for\n\n\n")
             return np.float32(0.0), True
             # spect = spect[:,215:215+646]
-        # spect = np.squeeze(spect)
         # print(spect.shape)
         return spect, False
     except Exception as err:
-        #tf.logging.warn(f"Error while computing features for {song_id}: {err}")
+        print("Error while computing features for")
         return np.float32(0.0), True
 
 
@@ -84,7 +86,7 @@ def get_model():
     # Define model architecture
     model = Sequential(
         [
-            InputLayer(input_shape=input_shape, name="input_layer"),
+            InputLayer(input_shape=INPUT_SHAPE, name="input_layer"),
 
             # BatchNormalization()
 
@@ -115,31 +117,32 @@ def get_model():
     )
     return model
 
-def compile_model(model, loss = 'binary_crossentropy', optimizer = 'sgd', metrics = ['accuracy']):
+
+def compile_model(model, loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy']):
     model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
 
 
 # Dataset pipelines
 def get_training_dataset(path):
     return get_dataset(path, shuffle=True,
-                       cache_dir=os.path.join(base_datasets_path,"/tmp/tf_cache/training/"))
+                       cache_dir=os.path.join(SOURCE_PATH, "/tmp/tf_cache/training/"))
 
 
 def get_validation_dataset(path):
-    return get_dataset(path , batch_size=32, shuffle=False,
-                       random_crop=False, cache_dir=os.path.join(base_datasets_path,"/tmp/tf_cache/validation/"))
+    return get_dataset(path, batch_size=32, shuffle=False,
+                       random_crop=False, cache_dir=os.path.join(SOURCE_PATH, "/tmp/tf_cache/validation/"))
 
 
 def get_test_dataset(path):
     return get_dataset(path, batch_size=50, shuffle=False,
-                       infinite_generator=False, cache_dir=os.path.join(base_datasets_path,"/tmp/tf_cache/test/"))
+                       infinite_generator=False, cache_dir=os.path.join(SOURCE_PATH, "/tmp/tf_cache/test/"))
 
 
 def get_dataset(input_csv, input_shape=INPUT_SHAPE, batch_size=32, shuffle=True,
-                infinite_generator=True, random_crop=False, cache_dir=os.path.join(base_datasets_path,"/tmp/tf_cache/"), num_parallel_calls=32):
+                infinite_generator=True, random_crop=False, cache_dir=os.path.join(SOURCE_PATH, "/tmp/tf_cache/"),
+                num_parallel_calls=32):
     # build dataset from csv file
     dataset = dp.dataset_from_csv(input_csv)
-
     # Shuffle data
     if shuffle:
         dataset = dataset.shuffle(buffer_size=100, seed=1, reshuffle_each_iteration=True)
@@ -167,11 +170,11 @@ def get_dataset(input_csv, input_shape=INPUT_SHAPE, batch_size=32, shuffle=True,
 
     # one hot encoding of labels
     dataset = dataset.map(lambda sample: dict(sample, binary_label=tf_multilabel_binarize(
-        sample.get("label", b""), label_list_tf=tf.constant(label_list))[0]), )
+        sample.get("label", b""), label_list_tf=tf.constant(LABELS_LIST))[0]), )
 
     # set output shape
     dataset = dataset.map(lambda sample: dict(sample, binary_label=dp.set_tensor_shape(
-        sample["binary_label"], (len(label_list)))))
+        sample["binary_label"], (len(LABELS_LIST)))))
 
     if infinite_generator:
         # Repeat indefinitly
@@ -185,8 +188,9 @@ def get_dataset(input_csv, input_shape=INPUT_SHAPE, batch_size=32, shuffle=True,
 
     return dataset
 
-def load_test_set_raw(LOADING_PATH = os.path.join(base_datasets_path,"GroundTruth/"),
-                      SPECTROGRAM_PATH = os.path.join(base_datasets_path,"MelSpectograms_top20/")):
+
+def load_test_set_raw(LOADING_PATH=os.path.join(SOURCE_PATH, "GroundTruth/"),
+                      SPECTROGRAM_PATH=SPECTROGRAMS_PATH):
     # Loading testset groundtruth
     test_ground_truth = pd.read_csv(os.path.join(LOADING_PATH, "test_ground_truth.csv"))
     all_ground_truth = pd.read_pickle(os.path.join(LOADING_PATH, "ground_truth_hot_vector.pkl"))
@@ -197,20 +201,21 @@ def load_test_set_raw(LOADING_PATH = os.path.join(base_datasets_path,"GroundTrut
     test_classes = all_ground_truth.values
     test_classes = test_classes.astype(int)
 
-    spectrograms = np.zeros([len(test_ground_truth), 1292, 96, 1])
+    spectrograms = np.zeros([len(test_ground_truth), 1292, 96])
     songs_ID = np.zeros([len(test_ground_truth), 1])
     for idx, filename in enumerate(list(test_ground_truth.song_id)):
         try:
             spect = np.load(os.path.join(SPECTROGRAM_PATH, str(filename) + '.npz'))['feat']
         except:
             continue
-        if (spect.shape == (96, 1292, 1)):
+        if (spect.shape == (1, 1292, 96)):
             spectrograms[idx] = spect
             songs_ID[idx] = filename
-    spectrograms = np.asarray(spectrograms)
-    return spectrograms,test_classes
+    spectrograms = np.expand_dims(spectrograms, axis=3)
+    return spectrograms, test_classes
 
-def evaluate_model(model,spectrograms,test_classes):
+
+def evaluate_model(model, spectrograms, test_classes, saving_path):
     """
     Evaluates a given model using accuracy, area under curve and hamming loss
     :param model: model to be evaluated
@@ -229,34 +234,42 @@ def evaluate_model(model,spectrograms,test_classes):
     # Hamming loss is the fraction of labels that are incorrectly predicted.
     hamming_error = hamming_loss(test_classes, test_pred)
     print("Hamming Loss (ratio of incorrect tags) is: " + str(hamming_error))
-    return accuracy,auc_roc,hamming_error
+    with open(os.path.join(saving_path, "evaluation_results.txt"), "w") as f:
+        f.write("Exact match accuracy is: " + str(accuracy) + "%\n" + "Area Under the Curve (AUC) is: " + str(auc_roc)
+                + "\nHamming Loss (ratio of incorrect tags) is: " + str(hamming_error))
+    print("saving prediction to disk")
+    np.savetxt(os.path.join(saving_path, 'predictions.out'), test_pred_prob, delimiter=',')
+    np.savetxt(os.path.join(saving_path, 'test_ground_truth_classes.txt'), test_classes, delimiter=',')
+    return accuracy, auc_roc, hamming_error
 
-def save_model(model,path):
+
+def save_model(model, path):
     # serialize model to JSON
     model_json = model.to_json()
     with open(os.path.join(path, "model.json"), "w") as json_file:
         json_file.write(model_json)
     # serialize weights to HDF5
-    model.save_weights(os.join.path(path,"model.h5"))
+    model.save_weights(os.join.path(path, "model.h5"))
     print("Saved model to disk")
 
+
 def main():
-    #splitting datasets
-    #split_dataset()
+    # splitting datasets
+    # split_dataset()
 
     # Loading datasets
-    training_dataset = get_training_dataset(os.path.join(base_datasets_path,"train_ground_truth.csv"))
-    val_dataset = get_validation_dataset(os.path.join(base_datasets_path,"validation_ground_truth.csv"))
+    training_dataset = get_training_dataset(os.path.join(SOURCE_PATH, "GroundTruth/train_ground_truth.csv"))
+    val_dataset = get_validation_dataset(os.path.join(SOURCE_PATH, "GroundTruth/validation_ground_truth.csv"))
 
     # TODO: path
-    exp_dir = os.path.join(base_datasets_path,"/experiments/")
+    exp_dir = os.path.join(SOURCE_PATH, "experiments/")
     experiment_name = os.path.join("Pipeline_CNN", strftime("%Y-%m-%d_%H-%M-%S", localtime()))
 
     fit_config = {
-        "steps_per_epoch": 10,
-        "epochs": 2,
+        "steps_per_epoch": 1000,
+        "epochs": 20,
         "initial_epoch": 0,
-        "validation_steps": 2,
+        "validation_steps": 100,
         "callbacks": [
             TensorBoard(log_dir=os.path.join(exp_dir, experiment_name)),
             ModelCheckpoint(os.path.join(exp_dir, experiment_name, "last_iter.h5"),
@@ -271,13 +284,17 @@ def main():
     # Printing the command to run tensorboard [Just to remember]
     print("Execute the following in a terminal:\n" + "tensorboard --logdir=" + os.path.join(exp_dir, experiment_name))
 
-    dp.safe_remove(os.path.join(base_datasets_path,'tmp/tf_cache/training/', "_0.lockfile"))
-    dp.safe_remove(os.path.join(base_datasets_path,'/tmp/tf_cache/validation/', "_0.lockfile"))
+    model = get_model()
+    compile_model(model)
+
+    dp.safe_remove(os.path.join(SOURCE_PATH, '/tmp/tf_cache/training/', "_0.lockfile"))
+    dp.safe_remove(os.path.join(SOURCE_PATH, '/tmp/tf_cache/validation/', "_0.lockfile"))
     model.fit(training_dataset, validation_data=val_dataset, **fit_config)
 
-    spectrograms,test_classes = load_test_set_raw()
-    accuracy, auc_roc, hamming_error = evaluate_model(model,spectrograms,test_classes)
-    save_model(model,"path/path/path")
+    spectrograms, test_classes = load_test_set_raw()
+    accuracy, auc_roc, hamming_error = evaluate_model(model, spectrograms, test_classes,
+                                                      saving_path=os.path.join(exp_dir, experiment_name))
+    # save_model(model,"path/path/path")
 
 
 if __name__ == "__main__":
